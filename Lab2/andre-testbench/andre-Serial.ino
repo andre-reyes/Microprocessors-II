2c9964697e53f580690142e4c6078022d022755a
Areyes <areyez92@gmail.com>
2023-10-26 23:35:41 -0400

    update to syntax in andre-Serial.ino

--- Lab2/andre-testbench/andre-Serial.ino
+++ Lab2/andre-testbench/andre-Serial.ino
@@ -1,16 +1,17 @@
 // Start of Serial read: reads if snake ate apple from snake.py
-// and write: sends joystick new_direction to snake.py
+// and write: sends joystick curr_dir to snake.py
 
 #define joystick_x A0
 #define joystick_y A1
-#define analog_range 1024
-int resting_threshold = analog_range/4;
-int joystick_center = analog_range/2;
-int x_dir = 0;
-int y_dir = 0;
+#define analog_range 1024 // general range of the analog input of arduino
+int resting_threshold = analog_range/4; //values within 25% of resting value of 500
+int joystick_center = analog_range/2; // at rest joystick hovers around 500
+int joystick_x_dir = 0; // x value of joystick (left: -1024 to -750, right: 750 to 1024)
+int joystick_y_dir = 0; // y value of joystick (down: -1024 to -750, up: 750 to 1024)
 int incomingByte = 0; 
-char new_direction = "";
-char old_direction = "";
+char curr_dir = "";  // current direction
+char prev_dir = ""; // previous direction
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -37,30 +38,25 @@ void loop() {
   //   }
   // }
 
-  x_dir = analogRead(joystick_x);
-  y_dir = analogRead(joystick_y);
 
-  // Serial.print("x = ");
-  // Serial.print(x_dir);
-  // Serial.print(", y = ");
-  // Serial.println(y_dir);
-  // delay(200);
-  
-  if(y_dir - joystick_center > resting_threshold){
-    new_direction = 'w';
+  // read joystick values and handle non-repeat ouput
+  joystick_x_dir = analogRead(joystick_x);
+  joystick_y_dir = analogRead(joystick_y);
+
+  if(joystick_y_dir - joystick_center > resting_threshold){
+    curr_dir = 'w';
   }
-  else if(abs(y_dir - joystick_center) > resting_threshold){
-    new_direction = 's';
+  else if(abs(joystick_y_dir - joystick_center) > resting_threshold){
+    curr_dir = 's';
   }
-  else if(x_dir - joystick_center > resting_threshold){
-    new_direction = 'a';
+  else if(joystick_x_dir - joystick_center > resting_threshold){
+    curr_dir = 'a';
   }
-  else if(abs(x_dir - joystick_center) > resting_threshold){
-    new_direction = 'd';
+  else if(abs(joystick_x_dir - joystick_center) > resting_threshold){
+    curr_dir = 'd';
   }
-  if(old_direction != new_direction){
-    old_direction = new_direction;
-    Serial.println(new_direction);
+  if(prev_dir != curr_dir){
+    prev_dir = curr_dir;
+    Serial.println(curr_dir);
   }
-  delay(100);
 }
